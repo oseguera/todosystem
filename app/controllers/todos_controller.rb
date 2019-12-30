b004e63bf76c3543a40b3fe8eae10dc2cbbc27a3
Emmanuel Oseguera <emmanuel.oseguera@magmalabs.io>
2019-12-30 09:28:07 -0600

    My final todo app working great

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,31 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.order(created_at: :desc)
+    @todo = Todo.new
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+
+    if todo.save
+      todo_html = render_to_string(partial: "todos/todo", 
+                  locals: {todo: todo}, formats: [:html])
+      return render(json: {id: todo.id, html: todo_html}, status: :created)
+    end
+
+    render json: {errors: todo.errors.to_h}, status: :unprocessable_entity
+  end
+
+  def destroy
+    todo = Todo.find_by(id: params[:id])
+    todo.destroy
+
+    render plain: "", status: :no_content
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name)
+  end
+end
---
+++ test/controllers/todos_controller_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class TodosControllerTest < ActionDispatch::IntegrationTest
+  test "GET /todos" do
+    get todos_path
+
+    assert_response :success
+  end
+
+  test "POST /todos (success)" do
+    post todos_path, params: {todo: {name: "Task example"}}, as: :json
+
+    assert_response :created
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    assert json_response.dig(:id).present?
+    assert json_response.dig(:html).present?
+  end
+
+  test "POST /todos (failure)" do
+    post todos_path, params: {todo: {name: ""}}, as: :json
+
+    assert_response :unprocessable_entity
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    assert json_response.dig(:errors, :name).present?
+  end
+  
+  test "DELETE /todos/:id" do
+    todo = todos(:todo)
+    delete todo_path(todo), as: :json
+
+    assert_response :no_content
+  end
+end
